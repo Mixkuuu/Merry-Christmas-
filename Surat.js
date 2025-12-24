345ebc6548905a80cb0ce47f3d4f1f8ffde8ae15
Mixkuuu <auahk398@gmail.com>
2025-12-24 21:30:31 +0700

    Add snow animation and open letter function

---
+++ Surat.js
@@ -0,0 +1,12 @@
+const snow = document.getElementById("snow");
+
+for(let i=0;i<60;i++){
+    let s=document.createElement("span");
+    s.style.left=Math.random()*100+"%";
+    s.style.animationDuration=5+Math.random()*5+"s";
+    snow.appendChild(s);
+}
+
+function openLetter(){
+    document.getElementById("letter").classList.add("show");
+}
